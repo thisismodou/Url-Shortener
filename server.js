51bf1bee24724bec9fd22bc9e60b0efed810d576
Modou Lo <modou@modous-mbp.lan>
2020-05-03 19:26:43 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const ShortUrl = require('./models/ShortUrl');
+const app = express();
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render('index', { shortUrls: shortUrls });
+});
+
+app.post('/shorten', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl });
+  res.redirect('/');
+});
+
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (!shortUrl) return res.sendStatus(404);
+
+  shortUrl.clicks++;
+  shortUrl.save();
+
+  res.redirect(shortUrl.full);
+});
+
+app.listen(process.env.PORT || 5000);
